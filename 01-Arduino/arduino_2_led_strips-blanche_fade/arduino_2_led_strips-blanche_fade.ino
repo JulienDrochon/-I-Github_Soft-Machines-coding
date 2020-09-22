929e3fce73b5c4d9ae25b539812900ff2b8aaa77
Julien <Julien@Laptop93.local>
2020-09-22 18:15:33 +0200

    Update arduino_2_led_strips-blanche_fade.ino

--- 01-Arduino/arduino_2_led_strips-blanche_fade/arduino_2_led_strips-blanche_fade.ino
+++ 01-Arduino/arduino_2_led_strips-blanche_fade/arduino_2_led_strips-blanche_fade.ino
@@ -1,25 +1,30 @@
 // which digital pin we're going to use
-#define PIN 9// how fast you want it to cycle, higher is slower
+#define PIN_LED_STRIP_01 9// pin arduino où est raccordé la première led strip = pin 9
+#define PIN_LED_STRIP_02 6// pin arduino où est raccordé la deuxième led strip = pin 6
 #define FADESPEED 7
 
+// déclaration variables
+int fondu01 = 0;
+int fondu02 = 255;
+
 void setup() {
-  Serial.begin(9600);
-  pinMode(PIN, OUTPUT);
+  pinMode(PIN_LED_STRIP_01, OUTPUT);
+  pinMode(PIN_LED_STRIP_02, OUTPUT);
 }
 
 
 void loop() {
-  int i;
+  if (fondu01 < 255) {
+    fondu01 = fondu01 + 1;
+    analogWrite(PIN_LED_STRIP_01, fondu01);
+  } else {
+    fondu01 = 0;
+  }
 
-  // fade from low to high (note we can't go to 256)
-  for (i = 0; i < 255; i++) {
-    analogWrite(PIN, i);
-    delay(FADESPEED);
-  }// then high to low
-  for (i = 255; i > 0; i--) {
-    analogWrite(PIN, i);
-    delay(FADESPEED);
-  }// pause in between
-  delay(50);
-
+  if (fondu02 > 0) {
+    fondu02 = fondu02 - 1;
+    analogWrite(PIN_LED_STRIP_02, fondu02);
+  } else {
+    fondue02 = 255;
+  }
 }
