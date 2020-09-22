6ea87a5650e020f19df46cc7c0b334afd2b41911
Julien <Julien@Laptop93.local>
2020-09-22 21:50:10 +0200

    Update

--- 01-Arduino/arduino_1_led_strip_blanche_fondu/arduino_1_led_strip_blanche_fondu.ino
+++ 01-Arduino/arduino_1_led_strip_blanche_fondu/arduino_1_led_strip_blanche_fondu.ino
@@ -1,25 +1,23 @@
-// which digital pin we're going to use
-#define PIN 9// how fast you want it to cycle, higher is slower
-#define FADESPEED 7
+//Déclaration de variables constantes
+#define PIN_LED_STRIP_01 9// pin arduino où est raccordé la première led strip = pin 9
+
+// déclaration variables
+int fondu01 = 0;
+int vitesseFondu = 50;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(PIN, OUTPUT);
+  //déclaration de la pin numérique 9 en tant que sortie
+  pinMode(PIN_LED_STRIP_01, OUTPUT);
 }
 
 
 void loop() {
-  int i;
-
-  // fade from low to high (note we can't go to 256)
-  for (i = 0; i < 255; i++) {
-    analogWrite(PIN, i);
-    delay(FADESPEED);
-  }// then high to low
-  for (i = 255; i > 0; i--) {
-    analogWrite(PIN, i);
-    delay(FADESPEED);
-  }// pause in between
-  delay(50);
-
+  //fondu lumineux de la led strip#1 branchée sur le pin 9 de l'arduino
+  if (fondu01 < 255) {
+    fondu01 = fondu01 + 1;
+    analogWrite(PIN_LED_STRIP_01, fondu01);
+  } else {
+    fondu01 = 0;
+  }
+  delay(vitesseFondu);
 }
--- 01-Arduino/arduino_2_led_strips-blanche_fondu/arduino_2_led_strips-blanche_fondu.ino
+++ 01-Arduino/arduino_2_led_strips-blanche_fondu/arduino_2_led_strips-blanche_fondu.ino
@@ -1,30 +1,36 @@
-// which digital pin we're going to use
+//Déclaration de variables constantes
 #define PIN_LED_STRIP_01 9// pin arduino où est raccordé la première led strip = pin 9
 #define PIN_LED_STRIP_02 6// pin arduino où est raccordé la deuxième led strip = pin 6
-#define FADESPEED 7
+//#define FADESPEED 7 // Vitesse
 
 // déclaration variables
 int fondu01 = 0;
 int fondu02 = 255;
+int vitesseFondus = 50;
 
 void setup() {
+  //déclaration de la pin numérique 9 en tant que sortie
   pinMode(PIN_LED_STRIP_01, OUTPUT);
+  //déclaration de la pin numérique 6 en tant que sortie
   pinMode(PIN_LED_STRIP_02, OUTPUT);
 }
 
 
 void loop() {
+  //fondu lumineux de la led strip #1 branchée sur le pin 9 de l'arduino
   if (fondu01 < 255) {
     fondu01 = fondu01 + 1;
     analogWrite(PIN_LED_STRIP_01, fondu01);
   } else {
     fondu01 = 0;
   }
-
+//fondu lumineux de la led strip #1 branchée sur le pin 9 de l'arduino
   if (fondu02 > 0) {
     fondu02 = fondu02 - 1;
     analogWrite(PIN_LED_STRIP_02, fondu02);
   } else {
-    fondue02 = 255;
+    fondu02 = 255;
   }
+  
+  delay(vitesseFondus);
 }
