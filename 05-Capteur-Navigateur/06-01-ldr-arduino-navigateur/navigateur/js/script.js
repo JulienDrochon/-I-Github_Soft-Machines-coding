59b53dad0bbfeaba38d94fb51eddfbde951f717b
JulienDrochon <41402969+JulienDrochon@users.noreply.github.com>
2020-10-16 12:57:39 +0200

    update

--- 05-Capteur-Navigateur/06-01-ldr-arduino-navigateur/navigateur/js/script.js
+++ 05-Capteur-Navigateur/06-01-ldr-arduino-navigateur/navigateur/js/script.js
@@ -1,3 +1,9 @@
 setInterval(function () {
-  document.getElementById("data").innerHTML = dataFromArduino[0];
+  let tailleTexte = mapData(dataFromArduino[0], 0, 400, 0, 40);
+  let interlettrage = mapData(dataFromArduino[1], 0, 400, 0, 40);
+
+  document.querySelector("#monTexteAchanger").style.fontSize =
+    tailleTexte + "px";
+  document.querySelector("#monTexteAchanger").style.fontSize =
+    interlettrage + "px";
 }, 50);
--- 05-Capteur-Navigateur/06-01-ldr-arduino-navigateur/navigateur/lib/dataFromArduino.js
+++ 05-Capteur-Navigateur/06-01-ldr-arduino-navigateur/navigateur/lib/dataFromArduino.js
@@ -10,3 +10,10 @@ socket.on("data", function (message) {
     dataFromArduino[i] = message.arduinodata[i];
   }
 });
+
+function mapData(value, a, b, c, d) {
+  // first map value from (a..b) to (0..1)
+  value = (value - a) / (b - a);
+  // then map it from (0..1) to (c..d) and return it
+  return c + value * (d - c);
+}
